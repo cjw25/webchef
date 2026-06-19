b64b8a2e4fbb388d61150d6d4779f786a7a2bf8f
mate12 <cnjzzan@naver.com>
2026-06-19 16:42:39 +0900

    Jeong Won

---
+++ src/main/java/com/example/fivechef/WebChef/WebChefApplication.java
@@ -0,0 +1,13 @@
+package com.example.fivechef.WebChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fivechef/WebChef/WebChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fivechef.WebChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
