fb2c63cc5ecbc833c4ba344310530edaafbc4f08
mate12 <cnjzzan@naver.com>
2026-06-29 14:27:41 +0900

    Jeong Won

--- src/main/java/com/example/fivechef/WebChef/controller/MainController.java
+++ src/main/java/com/example/fivechef/WebChef/controller/MainController.java
@@ -1,13 +1,26 @@
 package com.example.fivechef.WebChef.controller;
 
+import com.example.fivechef.WebChef.dto.CourseListResponse;
+import com.example.fivechef.WebChef.service.CourseService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
-    @GetMapping({"/", "/index"})
-    public String index() {
+    private final CourseService courseService;
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        List<CourseListResponse> popularCourses = courseService.getPopularCourseList();
+
+        model.addAttribute("popularCourses", popularCourses);
+
         return "index";
     }
 }
