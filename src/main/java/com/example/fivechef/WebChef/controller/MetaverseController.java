067979c28e2138ce1fc4d573c46540758578870f
mate12 <cnjzzan@naver.com>
2026-07-02 16:01:27 +0900

    Jeong Won
    Jeong Won

--- src/main/java/com/example/fivechef/WebChef/controller/MetaverseController.java
+++ src/main/java/com/example/fivechef/WebChef/controller/MetaverseController.java
@@ -1,11 +1,13 @@
 package com.example.fivechef.WebChef.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MetaverseController {
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/metaverse")
     public String metaversePage() {
         return "metaverse";
